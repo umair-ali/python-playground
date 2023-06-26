54d9ac7a8ae09b558d39b5b1d2787bbc2a7131c4
Umair Ali <Umair.Ali@spglobal.com>
2023-06-26 16:50:00 +0500

    basic fast api application

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # python-playground
 A playground for my python learning journey
+
+
+## Fast API demo
+Source: https://towardsdatascience.com/setting-up-python-projects-part-i-408603868c08
+How to run,
+  - `poetry shell`
+  - `python src/py_playground/app.py`
+  - Test at: http://localhost:9090/docs
+
+
+
---
+++ src/py_playground/app.py
@@ -0,0 +1,44 @@
+import httpx
+import uvicorn
+from fastapi import FastAPI, HTTPException, Path, status
+from httpx import Response
+import requests
+
+
+# app
+app: FastAPI = FastAPI()
+
+data = { "index": -1, "value": "hello fast api!"}
+@app.get(path="/api/v1/data/{number}", status_code=status.HTTP_200_OK)
+def get_data(
+        number: int = Path(title="The data to get (based on number)", ge=1, le=15)
+) -> list:
+    result = []
+    for x in range(number):
+        result.append({ "index": x, "value": "hello fast api!"})
+    return result
+
+
+@app.get(path="/api/v1/pokemon/{number}", status_code=status.HTTP_200_OK)
+def get_pokemon(
+        number: int = Path(title="The Pokémon to get (based on number)", ge=1, le=151)
+) -> dict:
+    pokemon_url: str = f"https://pokeapi.co/api/v2/pokemon/{number}"
+
+    try:
+        response = httpx.get(url=pokemon_url)
+    except Exception as e:
+        print(e)
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=f"Could not send a request to {pokemon_url}, {e}",
+        )
+
+    if response.status_code == status.HTTP_200_OK:
+        return response.json()
+    else:
+        raise HTTPException(status_code=response.status_code, detail=response.text)
+
+
+if __name__ == "__main__":
+    uvicorn.run(app="app:app", host="127.0.0.1", port=9090, reload=True)
